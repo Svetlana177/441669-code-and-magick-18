644d24e471026d8e3b1001bc21aa418ed96c54f0
Svetlana177 <svetlanarozhkova177@mail.ru>
2019-09-25 22:25:51 +0300

    fix third task "us Horde" in code and magik;

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,5 @@
 'use strict';
-var WIZARDS_OBJECT = 4;
+var WIZARDS_COUNT = 4;
 var WIZARDS_NAMES = ['Иван', 'Хуан', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARDS_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COATS_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
@@ -10,14 +10,14 @@ userDialog.classList.remove('hidden');
 document.querySelector('.setup-similar').classList.remove('hidden');
 var similarListElement = document.querySelector('.setup-similar-list'); // Покажем блок с похожими персонажами.
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item'); // Найдём шаблон, который мы будем копировать. template элемент, в который мы будем вставлять похожих магов.
-// Случайное число из массива
-var getRandom = function (rand) {
-  return rand[Math.floor(Math.random() * rand.length)];
+// Случайный элемент из массива
+var getRandom = function (elements) {
+  return elements[Math.floor(Math.random() * elements.length)];
 };
 // Генерирует волшебника с рандомными свойствами
 var getRandomWizard = function () {
   var wizard = {
-    name: [getRandom(WIZARDS_NAMES)] + ' ' + [getRandom(WIZARDS_SURNAMES)],
+    name: getRandom(WIZARDS_NAMES) + ' ' + getRandom(WIZARDS_SURNAMES),
     coatColor: getRandom(COATS_COLOR),
     eyesColor: getRandom(EYES_COLOR)
   };
@@ -25,7 +25,7 @@ var getRandomWizard = function () {
   return wizard;
 };
 // цикл перебора волшебников от 1 до 4
-for (var i = 0; i < WIZARDS_OBJECT; i++) {
+for (var i = 0; i < WIZARDS_COUNT; i++) {
   wizards[i] = getRandomWizard();
 }
 // создайте DOM-элементы, соответствующие случайно сгенерированным волшебникам и заполните их данными из массива:
